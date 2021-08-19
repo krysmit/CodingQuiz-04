55aed1d708118bc055880c324f07c3745f3b70b5
Krystyn Smith <krskrsmith@gmail.com>
2021-08-18 20:44:32 -0500

    working on the questions and getting buttons to work

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,14 @@ var chosenAnswer //var to collect which answer was chosen by user
 var rootEl = $('#root'); //the quiz box where intro, Qs and As will appear
 var quesEl = $('<h2>');
 var ansEl = $('<p>');
-
+var quizbox = document.getElementById("quizbox");
+var question = document.getElementById("question");
+var choice1 = document.getElementById("choice-1");
+var choice2 = document.getElementById("choice-2");
+var choice3 = document.getElementById("choice-3");
+var choice4 = document.getElementById("choice-4");
+var mainbutton = document.querySelector(".mainbutton")
+quizbox.style.display = "none";
 
 //NEEDS MORE WORK - countdown working, but not showing
 // Timer that counts down from 60
@@ -35,6 +42,9 @@ quesEl.append(ansEl);
 rootEl.append(quesEl);
 
 //when button is clicked, next question appears and title page disappears
+
+
+
 $("#hide").click(function(){
   $("p").hide();
 });
@@ -44,41 +54,45 @@ $("#show").click(function(){
 });
 
 
-//questions
-//Q1
-//Commonly used data types DO NOT include:
-//1. strings
-//2. booleans
-//3. alerts
-//4. numbers
+var questions = [
+  {
+Q1:"Commonly used data types DO NOT include:",
+ch1:"strings",
+ch2:"booleans",
+ch3:"alerts",
+ch4:"numbers",
+ans:3
+  },
+{
+Q2:"The condition in an if/else statement is enclosed with _____.",
+ch1:"quotes",
+ch2:"curly brackets",
+ch3:"parantheses",
+ch4:"square brackets",
+ans:3
+}
+]
 
-//Q2
-//The condition in an if/else statement is enclosed with _____.
-//1. quotes
-//2. curly brackets
-//3. parantheses
-//4. square brackets
+// Q3
+// Arrays in JavaScript can be used to store _____.
+// 1. numbers and strings
+// 2. other arrays
+// 3. booleans
+// 4. all of the above
 
-//Q3
-//Arrays in JavaScript can be used to store _____.
-//1. numbers and strings
-//2. other arrays
-//3. booleans
-//4. all of the above
+// Q4
+// String values must be enclosed within _______ when being assigned to variables.
+// 1. commas
+// 2. curly brackets
+// 3. quotes
+// 4. parentheses
 
-//Q4
-//String values must be enclosed within _______ when being assigned to variables.
-//1. commas
-//2. curly brackets
-//3. quotes
-//4. parentheses
-
-//Q5
-//A very useful tool used during development and debugging for printing content to the debugger is:
-//1. JavaScript
-//2. terminal/bash
-//3. for loops
-//4. console.log
+// Q5
+// A very useful tool used during development and debugging for printing content to the debugger is:
+// 1. JavaScript
+// 2. terminal/bash
+// 3. for loops
+// 4. console.log
 
 
 
